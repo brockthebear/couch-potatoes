0ff4861ec36278f93be3f0b756807d43f1b49370
Brock Boren <brock.boren@gmail.com>
2018-02-10 21:11:31 -0600

    Create User model.
    This also generates the first migration for our db.

---
+++ server/migrations/20180211025947-create-user.js
@@ -0,0 +1,34 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        first_name: {
+            type: Sequelize.STRING,
+        },
+        last_name: {
+            type: Sequelize.STRING,
+        },
+        email: {
+            type: Sequelize.STRING,
+        },
+        password: {
+            type: Sequelize.STRING,
+        },
+        settings: {
+            type: Sequelize.JSON,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
+};
---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('User', {
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        settings: DataTypes.JSON,
+    }, {
+        classMethods: {
+            associate: function(models) {
+        // associations can be defined here
+            },
+        },
+    });
+    return User;
+};
