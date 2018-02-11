b40c8e08cd0263dd75de181940aafa3943727e50
Brock Boren <brock.boren@gmail.com>
2018-02-10 19:10:24 -0600

    Include nodemon package to watch for changes to backend.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
-const compression = require("compression");
+require('dotenv').config();
 const express = require("express");
-
+const http = require('http');
 const path = require("path");
+
+// express modules.
+const bodyParser = require('body-parser');
+const compression = require("compression");
+const logger = require('morgan');
+
+const app = express();
 
 const globals = require("./globals");
 
 // Must come before render
 Object.assign(global, globals);
-
 const render = require("./render");
 
-const port = process.env.PORT || 3000;
-const app = express();
+const port = process.env.DB_PORT || 3000;
+app.set('port', port);
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(compression());
 
@@ -19,4 +32,6 @@ app.get("/", render);
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
 app.get("*", render);
 
-app.listen(port, () => console.log(`Server started on port: ${port}`));
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`Server started on port: ${port}`);
+});
